cde456e38e9a40a34c43d1f0a1f25298880d24af
pinky1018 <58349430+pinky1018@users.noreply.github.com>
2019-11-30 11:14:59 +0800

    Add files via upload

---
+++ food_tracker/view meal.py
@@ -0,0 +1,54 @@
+<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+</body>
+	<h1> view meal</h1>
+
+	<table> ="table table-bordered table-striped"
+		<thead>
+			<tr>
+				<th>Food name</th>
+				<th>{{meal_form.food name}}</th>
+			</tr>
+			<tr>
+				<th>Calories</th>
+				<th>{{meal_form.Calories}}</th>
+			</tr>
+			<tr>
+				<th>Serving size</th>
+				<th>{{meal_form.Serving size}}</th>
+			</tr>
+			<tr>
+				<th>Total Fat</th>
+				<th>{{meal_form.Total Fat}}</th>
+			</tr>
+			<tr>
+				<th>Saturated Fat </th>
+				<th>{{meal_form.Saturated Fat}}</th>
+			</tr>
+			<tr>
+				<th>Cholesterol</th>
+				<th>{{meal_form.Cholesterol}}</th>
+			</tr>
+			<tr>
+				<th>Soduim</th>
+				<th>{{meal_form.Soduim}}</th>
+			</tr>
+			<tr>
+				<th>Total Carbohydrade</th>
+				<th>{{meal_form.Carbohydrade}}</th>
+			</tr>
+			<tr>
+				<th>Dietary Fibre</th>
+				<th>{{meal_form.Dietary Fibre}}</th>
+			</tr>
+			<tr>
+				<th>Sugar</th>
+			</tr>
+			<tr>
+				<th>Protien</th>
+				<th>{{meal_form.Protien}}</th>
+			</tr>
+		</thead>
+	</table>		
+</body>		
